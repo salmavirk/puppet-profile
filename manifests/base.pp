5d2926dcc0e5b69d6c911aa1d6da8b6a11278a18
root <root@salma-virk-bd51f5575.mylabserver.com>
2018-06-20 13:42:07 +0000

    base.pp

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,10 +1,10 @@
-class prpfile::base(
+class profile::base(
   $ntp_servers = [
     '0.ubuntu.pool.ntp.org',
     '0.ubuntu.pool.ntp.org',
   ],
 )  {
-  class { '""ntp':
+  class { '::ntp':
     servers => $ntp_servers,
   }
 }
